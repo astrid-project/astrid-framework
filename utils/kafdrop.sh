80867cd4b5e9992fd1c57aac766794e3f44963f6
Alex Carrega <contact@alexcarrega.com>
2020-11-14 18:40:45 +0100

    kafdrop script

--- utils/kafdrop.sh
+++ utils/kafdrop.sh
@@ -4,6 +4,6 @@
 
 KAFKA_ENDPOINT=localhost
 
-docker run --restart=always -it --add-host kafka:KAFKA_ENDPOINT -d -p 9000:9000 \
-    -e KAFKA_BROKERCONNECT=KAFKA_ENDPOINT:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
+docker run --restart=always -it --add-host kafka:$KAFKA_ENDPOINT -d -p 9000:9000 \
+    -e KAFKA_BROKERCONNECT=$KAFKA_ENDPOINT:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
     -e SERVER_SERVLET_CONTEXTPATH="/" --name kafdrop obsidiandynamics/kafdrop
