eee8625bcb1de3aeb966dd05e2836b3472269b06
Alex Carrega <contact@alexcarrega.com>
2020-11-14 23:03:42 +0100

    Script update

--- utils/grafana.sh
+++ utils/grafana.sh
@@ -2,24 +2,11 @@
 # ASTRID
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
-
 if [ -z "$1" ]; then
-    echo "Error: missing Elasticsearch endpoint"
-    echo "Usage: $0 <elasticsearch-endpoint> [<grafana-port>]"
-    exit 1
-fi
-
-if [ -z "$2" ]; then
     GRAFANA_PORT=3000
 else
-    GRAFANA_PORT=$2
+    GRAFANA_PORT=$1
 fi
 
-ELASTICSEARCH_ENDPOINT=$1
-
 docker run -d -p $GRAFANA_PORT:3000 \
-    -e DS_NAME="Elasticsearch" -e DS_TYPE="elasticsearch" \
-    -e DS_URL="http://$ELASTICSEARCH_ENDPOINT" \
-    -e GF_USER="admin" -e GF_PASS="astrid" \
-    --name=grafana -v grafana-storage:/var/lib/grafana qapps/grafana-docker:master
-
+    --name=grafana -v grafana-storage:/var/lib/grafana grafana/grafana
