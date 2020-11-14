08cff354f63ab3ccb677234a541057bd7fa01665
Alex Carrega <contact@alexcarrega.com>
2020-11-14 18:46:26 +0100

    kafdrop script

--- utils/kafdrop.sh
+++ utils/kafdrop.sh
@@ -2,8 +2,14 @@
 # ASTRID
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
-KAFKA_ENDPOINT=localhost
+if [ -z "$1" ]; then
+    echo "Error: missing Kafka IP address"
+    echo "Usage: $0 <kafka-ip-address>"
+    exit 1
+fi
 
-docker run --restart=always -it --add-host kafka:$KAFKA_ENDPOINT -d -p 9000:9000 \
-    -e KAFKA_BROKERCONNECT=$KAFKA_ENDPOINT:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
+KAFKA_IP_ADDRESS=$1
+
+docker run --restart=always -it --add-host kafka:$KAFKA_IP_ADDRESS -d -p 9000:9000 \
+    -e KAFKA_BROKERCONNECT=$KAFKA_IP_ADDRESS:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
     -e SERVER_SERVLET_CONTEXTPATH="/" --name kafdrop obsidiandynamics/kafdrop
