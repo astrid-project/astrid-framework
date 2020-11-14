ea60e53c1fe5a0d11b9f4c6987c55d2ee8eb5559
Alex Carrega <contact@alexcarrega.com>
2020-11-14 19:00:58 +0100

    kafdrop script

--- utils/kafdrop.sh
+++ utils/kafdrop.sh
@@ -8,8 +8,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ -z "$2" ]; then
+    KAFDROP_PORT=9000
+else
+    KAFDROP_PORT=9001
+fi
+
 KAFKA_IP_ADDRESS=$1
 
-docker run --restart=always -it --add-host kafka:$KAFKA_IP_ADDRESS -d -p 9000:9000 \
+docker run --restart=always -it --add-host kafka:$KAFKA_IP_ADDRESS -d -p $KAFDROP_PORT:9000 \
     -e KAFKA_BROKERCONNECT=$KAFKA_IP_ADDRESS:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
     -e SERVER_SERVLET_CONTEXTPATH="/" --name kafdrop obsidiandynamics/kafdrop
