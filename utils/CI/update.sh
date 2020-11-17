949f4c949b3335d5b07c6e49532e4463a72507fe
Alex Carrega <contact@alexcarrega.com>
2020-11-17 19:01:11 +0100

    Script update

--- utils/CI/update.sh
+++ utils/CI/update.sh
@@ -7,9 +7,26 @@ CB_PATH="/opt/cb-manager/"
 
 if [ "$1" == "cb-manager" ]; then
     echo "$1 - Update repo"
+    rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
+
     cd $CB_PATH
-    git checkout '*'
-    git pull
+    git checkout '*' > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
+    git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
+
+    ACTIONS="checkout pull"
+    MODE="out err"
+
+    for action in $ACTIONS; do
+            for mode in $MODE; do
+                    echo $action $mode
+                    CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
+                    if [ ! -z "$CONTENT" ]; then
+                            location="CNIT"
+                            [ -f "$HOME/at-azure" ] && location="azure"
+                            bash $WORK_DIR/../utils/send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
+                    fi
+            done
+    done
 
     echo "$1 - Restart"
     bash "$WORK_DIR/service.sh" "$1" stop
