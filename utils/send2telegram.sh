7c958ee6d81749674153e6611754177ab16dfae1
Alex Carrega <contact@alexcarrega.com>
2020-11-17 12:05:58 +0100

    Telegram notification update

--- utils/send2telegram.sh
+++ utils/send2telegram.sh
@@ -9,6 +9,6 @@ if [ -z "$1" ]; then
 fi
 
 TELEGRAM_TOKEN="1463515955:AAHPW75lMcnOUfD2dZEilrW1d0HwQgcyGt4"
-TELEGRAM_CHATID="ASTRID"
+TELEGRAM_CHATID="429595417"
 
 curl -s -X POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage -d chat_id=$TELEGRAM_CHATID -d text="$1"
