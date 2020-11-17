288aa2676205638f34749dcf9bd8faa597b911d0
Alex Carrega <contact@alexcarrega.com>
2020-11-17 23:05:57 +0100

    CI script update

--- utils/CI/update.sh
+++ utils/CI/update.sh
@@ -6,31 +6,33 @@ WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CB_PATH="/opt/cb-manager/"
 
 if [ "$1" == "cb-manager" ]; then
-    echo "$1 - Update repo"
-    rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
+	echo "$1 - Update repo"
+	rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
 
-    cd $CB_PATH
-    git checkout '*' > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
-    git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
+	cd $CB_PATH
+	git checkout '*' > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
+	git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
 
-    ACTIONS="checkout pull"
-    MODE="out err"
+	ACTIONS="checkout pull"
+	MODE="out err"
 
-    for action in $ACTIONS; do
-            for mode in $MODE; do
-                    echo $action $mode
-                    CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
-                    if [ ! -z "$CONTENT" ]; then
-                            location="CNIT"
-                            [ -f "$HOME/at-azure" ] && location="azure"
-                            bash $WORK_DIR/../utils/send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
-                    fi
-            done
-    done
+	for action in $ACTIONS; do
+		for mode in $MODE; do
+			echo $action $mode
+			CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
+			if [ ! -z "$CONTENT" ]; then
+				location="unknown"
+				[ -f "$HOME/at-azure" ] && location="azure"
+				[ -f "$HOME/at-cnit_openstack" ] && location="CNIT-openstack"
+				[ -f "$HOME/at-cnit_k8s" ] && location="CNIT-k8s"
+				bash $WORK_DIR/../utils/send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
+			fi
+		done
+	done
 
-    echo "$1 - Restart"
-    bash "$WORK_DIR/service.sh" "$1" stop
-    bash "$WORK_DIR/service.sh" "$1" start
+	echo "$1 - Restart"
+	bash "$WORK_DIR/service.sh" "$1" stop
+	bash "$WORK_DIR/service.sh" "$1" start
 else
-    echo "Error: unknown service, must be: cb-manager"
+	echo "Error: unknown service, must be: cb-manager"
 fi
