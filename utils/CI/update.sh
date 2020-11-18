679fe80f1408f2c4742cf41ef009ef36cd52d0b5
Alex Carrega <contact@alexcarrega.com>
2020-11-18 11:52:13 +0100

    Script update

--- utils/CI/update.sh
+++ utils/CI/update.sh
@@ -7,11 +7,11 @@ CB_PATH="/opt/cb-manager/"
 
 if [ "$1" == "cb-manager" ]; then
 	echo "$1 - Update repo"
-	rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
+	rm -f "$HOME/log/checkout-*.log" "$HOME/log/pull-*.log"
 
-	cd $CB_PATH
-	git checkout '*' > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
-	git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
+	cd "$CB_PATH"
+	git checkout '*' > "$HOME/log/checkout-out.log" 2> "$HOME/log/checkout-err.log"
+	git pull > "$HOME/log/pull-out.log" 2> "$HOME/log/pull-err.log"
 
 	ACTIONS="checkout pull"
 	MODE="out err"
@@ -19,13 +19,16 @@ if [ "$1" == "cb-manager" ]; then
 	for action in $ACTIONS; do
 		for mode in $MODE; do
 			echo $action $mode
-			CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
+
+			CONTENT="$(cat \"$HOME/log/${action}-${mode}.log\")"
 			if [ ! -z "$CONTENT" ]; then
 				location="unknown"
 				[ -f "$HOME/at-azure" ] && location="azure"
 				[ -f "$HOME/at-cnit_openstack" ] && location="CNIT-openstack"
 				[ -f "$HOME/at-cnit_k8s" ] && location="CNIT-k8s"
-				bash $WORK_DIR/../send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
+
+				echo "Send notification via Telegram"
+				bash "$WORK_DIR/../send2telegram.sh" "@${location} ${action} - ${mode}: ${CONTENT}"
 			fi
 		done
 	done
@@ -33,6 +36,9 @@ if [ "$1" == "cb-manager" ]; then
 	echo "$1 - Restart"
 	bash "$WORK_DIR/service.sh" "$1" stop
 	bash "$WORK_DIR/service.sh" "$1" start
+
+	echo "Send notification via Telegram"
+	bash "$WORK_DIR/../send2telegram.sh" "@cnit-openstack: $(screen -ls)"
 else
 	echo "Error: unknown service, must be: cb-manager"
 fi
--- utils/update-this.sh
+++ utils/update-this.sh
@@ -6,11 +6,11 @@ FRAMEWORK_DIR=astrid-framework
 
 #--------------------------------------------------
 
-rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
+rm -f "$HOME/log/checkout-*.log" "$HOME/log/pull-*.log"
 
-cd $HOME/$FRAMEWORK_DIR
-git checkout "*" > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
-git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
+cd "$HOME/$FRAMEWORK_DIR"
+git checkout "*" > "$HOME/log/checkout-out.log" 2> "$HOME/log/checkout-err.log"
+git pull > "$HOME/log/pull-out.log" 2> "$HOME/log/pull-err.log"
 
 ACTIONS="checkout pull"
 MODE="out err"
@@ -18,13 +18,16 @@ MODE="out err"
 for action in $ACTIONS; do
 	for mode in $MODE; do
 		echo $action $mode
-		CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
+
+		CONTENT="$(cat \"$HOME/log/${action}-${mode}.log\")"
 		if [ ! -z "$CONTENT" ]; then
 			location="unknown"
 			[ -f "$HOME/at-azure" ] && location="azure"
 			[ -f "$HOME/at-cnit_openstack" ] && location="CNIT-openstack"
 			[ -f "$HOME/at-cnit_k8s" ] && location="CNIT-k8s"
-			bash $HOME/$FRAMEWORK_DIR/utils/send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
+
+			echo "Send notification via Telegram"
+			bash "$HOME/$FRAMEWORK_DIR/utils/send2telegram.sh" "@${location} ${action} - ${mode}: ${CONTENT}"
 		fi
 		echo -e "\n"
 	done
