2e636e7e0d53fb73655ec202990d54903cda122e
Alex Carrega <contact@alexcarrega.com>
2020-11-18 11:36:58 +0100

    Script update

---
+++ utils/update-this.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# ASTRID
+# author: Alex Carrega <alessandro.carrega@cnit.it>
+
+FRAMEWORK_DIR=astrid-framework
+
+#--------------------------------------------------
+
+rm -f $HOME/log/checkout-*.log $HOME/log/pull-*.log
+
+cd $HOME/$FRAMEWORK_DIR
+git checkout "*" > $HOME/log/checkout-out.log 2> $HOME/log/checkout-err.log
+git pull > $HOME/log/pull-out.log 2> $HOME/log/pull-err.log
+
+ACTIONS="checkout pull"
+MODE="out err"
+
+for action in $ACTIONS; do
+	for mode in $MODE; do
+		echo $action $mode
+		CONTENT="$(cat $HOME/log/${action}-${mode}.log)"
+		if [ ! -z "$CONTENT" ]; then
+			location="unknown"
+			[ -f "$HOME/at-azure" ] && location="azure"
+			[ -f "$HOME/at-cnit_openstack" ] && location="CNIT-openstack"
+			[ -f "$HOME/at-cnit_k8s" ] && location="CNIT-k8s"
+			bash $HOME/$FRAMEWORK_DIR/utils/send2telegram.sh "@${location} ${action} - ${mode}: ${CONTENT}"
+		fi
+		echo -e "\n"
+	done
+done
