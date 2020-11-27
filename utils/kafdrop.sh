9554653b9110f0fa6aba9fceab06e1fe3c11e37e
Alex Carrega <contact@alexcarrega.com>
2020-11-27 18:25:09 +0100

    Kafdrop util

--- utils/kafdrop.sh
+++ utils/kafdrop.sh
@@ -2,15 +2,30 @@
 # ASTRID
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
-if [ -z "$1" ]; then
-    echo "Error: missing Kafka IP address"
-    echo "Usage: $0 <kafka-ip-address>"
-    exit 1
+usage() {
+    echo "Usage: $0 <kafdrop-port> <kafka-ip-address> <kafka-port>"
+    exit $1
+}
+
+if [ -z "$1" ] ; then
+    echo "Error: missing Kafdrop port"
+    usage 1
 fi
 
-KAFDROP_PORT=9001
-KAFKA_IP_ADDRESS=$1
+if [ -z "$2" ] ; then
+    echo "Error: missing Kafka IP address"
+    usage 2
+fi
+
+if [ -z "$3" ] ; then
+    echo "Error: missing Kafka port"
+    usage 3
+fi
+
+KAFDROP_PORT=$1
+KAFKA_IP_ADDRESS=$2
+KAFKA_IP_PORT=$3
 
 docker run --restart=always -it --add-host kafka:$KAFKA_IP_ADDRESS -d -p $KAFDROP_PORT:9000 \
-    -e KAFKA_BROKERCONNECT=$KAFKA_IP_ADDRESS:9092 -e JVM_OPTS="-Xms32M -Xmx64M" \
+    -e KAFKA_BROKERCONNECT=$KAFKA_IP_ADDRESS:$KAFKA_IP_PORT -e JVM_OPTS="-Xms32M -Xmx64M" \
     -e SERVER_SERVLET_CONTEXTPATH="/" --name kafdrop obsidiandynamics/kafdrop
