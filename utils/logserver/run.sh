f8d0c6cb049c704a103aa8befe268c0390c3840c
Alex Carrega <contact@alexcarrega.com>
2020-11-28 22:11:09 +0100

    Logserver tool

---
+++ utils/logserver/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ASTRID
+# author: Alex Carrega <alessandro.carrega@cnit.it>
+
+WORK_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+usage() {
+    echo "Usage: $0 <logserver-port>"
+    exit $1
+}
+
+if [ -z "$1" ] ; then
+    echo "Error: missing Logserver port"
+    usage 1
+fi
+
+LOGSERVER_PORT=$1
+
+docker run -d --restart always --name logserver --net host \
+    -v "$WORK_PATH/config.json:/logserver:json \
+    stratoscale/logserver -addr :$LOGSERVER_PORT
