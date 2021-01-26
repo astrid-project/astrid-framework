485a3d72c19c56430eb3ba5f6d6c50318c16ca5f
Alex Carrega <contact@alexcarrega.com>
2021-01-26 11:42:41 +0100

    Supervisor management

--- utils/supervisor/superslacker.sh
+++ utils/supervisor/superslacker.sh
@@ -3,11 +3,19 @@
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
 DATE=$(date)
+SLACK_WEBHOOK_FILE="/home/debian/.slack_webhook_url"
 
-if [ -z "$SLACK_WEBHOOK_URL" ]; then
-    echo "[$DATE] Error: SLACK_WEBHOOK_URL not defined"
+if [ ! -f "$SLACK_WEBHOOK_FILE" ]; then
+    echo "[$DATE] Error: SLACK_WEBHOOK_FILE not found"
     exit 1
 fi
 
+SLACK_WEBHOOK_URL="$(cat $SLACK_WEBHOOK_FILE)"
+
+if [ -z "$SLACK_WEBHOOK_URL" ]; then
+    echo "[$DATE] Error: SLACK_WEBHOOK_URL not defined"
+    exit 2
+fi
+
 echo "[$DATE] Start superslacker"
-superslacker --webhook="$SLACK_WEBHOOK_URL" --hostname="cb.astrid.openstack.cnit" --events="STOPPED,STARTING,RUNNING,BACKOFF,STOPPING,EXITED,FATAL,UNKNOWN" --user="astrid-project"
+superslacker --webhook="$SLACK_WEBHOOK_URL" --hostname="cb.guard.openstack.cnit" --events="STOPPED,STARTING,RUNNING,BACKOFF,STOPPING,EXITED,FATAL,UNKNOWN" --user="guard-project"
