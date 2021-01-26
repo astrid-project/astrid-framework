8780d794b8efe2fa15676a96eb2944fb38f525ed
Alex Carrega <contact@alexcarrega.com>
2021-01-26 11:12:39 +0100

    Supervisor management

--- utils/supervisor/superslacker.sh
+++ utils/supervisor/superslacker.sh
@@ -2,9 +2,12 @@
 # Copyright (c) ASTRID 2020-2022
 # author: Alex Carrega <alessandro.carrega@cnit.it>
 
+DATE=$(date)
+
 if [ -z "$SLACK_WEBHOOK_URL" ]; then
-    echo "Error: SLACK_WEBHOOK_URL not defined"
+    echo "[$DATE] Error: SLACK_WEBHOOK_URL not defined"
     exit 1
 fi
 
+echo "[$DATE] Start superslacker"
 superslacker --webhook="$SLACK_WEBHOOK_URL" --hostname="cb.astrid.openstack.cnit" --events="STOPPED,STARTING,RUNNING,BACKOFF,STOPPING,EXITED,FATAL,UNKNOWN" --user="astrid-project"
