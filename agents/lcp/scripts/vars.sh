a42828403332e4ba76820aae0744bbe0b988ea31
Alex Carrega <contact@alexcarrega.com>
2021-03-29 10:48:03 +0200

    Version update for Dockerfile

--- agents/lcp/scripts/vars.sh
+++ agents/lcp/scripts/vars.sh
@@ -14,13 +14,5 @@ set_var INSTALLATION_PATH /opt/$COMPONENT
 set_var TMP_PATH /tmp
 set_var PIDFILE $TMP_PATH/$COMPONENT.pid
 
-set_var LCP_HOST 0.0.0.0
-set_var LCP_PORT 4000
-set_var LCP_AUTH_MAX_TTL 10min
-set_var LCP_POLYCUBE_HOST localhost
-set_var LCP_POLYCUBE_PORT 9000
-set_var LCP_POLYCUBE_TIMEOUT 20s
-set_var LCP_DEV_USERNAME lcp
-# Password: "astrid" hashed in sha256
-set_var LCP_DEV_PASSWORD 9c804f2550e31d8f98ac9b460cfe7fbfc676c5e4452a261a2899a1ea168c0a50
-set_var LCP_LOG_LEVEL DEBUG
+WORK_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$WORK_PATH/../settings/$VERSION/.env"
--- platform/cb-manager/scripts/vars.sh
+++ platform/cb-manager/scripts/vars.sh
@@ -14,19 +14,5 @@ set_var INSTALLATION_PATH /opt/$COMPONENT
 set_var TMP_PATH /tmp
 set_var PIDFILE $TMP_PATH/$COMPONENT.pid
 
-set_var CB_MAN_HOST 0.0.0.0
-set_var CB_MAN_PORT 5000
-set_var CB_MAN_AUTH true
-set_var CB_MAN_HTTPS false
-set_var CB_MAN_HEARTBEAT_TIMEOUT 10s
-set_var CB_MAN_HEARTBEAT_PERIOD 1min
-set_var CB_MAN_HEARTBEAT_AUTH_EXPIRATION 5min
-set_var CB_MAN_ELASTICSEARCH_ENDPOINT localhost:9200
-set_var CB_MAN_ELASTICSEARCH_TIMEOUT 20s
-set_var CB_MAN_ELASTICSEARCH_RETRY_PERIOD 1min
-set_var CB_MAN_ELASTIC_APM_ENABLED false
-set_var CB_MAN_ELASTIC_APM_SERVER http://localhost:8200
-set_var CB_MAN_DEV_USERNAME cb-manager
-# Password: "astrid" hashed in sha256
-set_var CB_MAN_DEV_PASSWORD 9c804f2550e31d8f98ac9b460cfe7fbfc676c5e4452a261a2899a1ea168c0a50
-set_var CB_MAN_LOG_LEVEL DEBUG
+WORK_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$WORK_PATH/../settings/$VERSION/.env"
