560857455c09e16aa3962951089c71b232e7836b
Alex Carrega <contact@alexcarrega.com>
2021-04-23 11:16:11 +0200

    Kafka version update

--- platform/kafka/scripts/vars.sh
+++ platform/kafka/scripts/vars.sh
@@ -8,7 +8,7 @@ set_var() {
 
 set_var COMPONENT kafka
 set_var COMPONENT_DEP zookeeper
-set_var VERSION 2.6.0
+set_var VERSION 2.6.2
 set_var SCALA_VERSION 2.12
 set_var INSTALLATION_PATH /opt/$COMPONENT
 set_var SOURCE ${COMPONENT}_$SCALA_VERSION-$VERSION
